9afd1771c71190c8e35ae30921f386ebae56bc31
12345 <ing2895@naver.com>
2020-06-04 13:06:09 +0900

    no message

---
+++ programmers/코딩테스트 고득점 Kit/LV1/LV1_문자열 내 p와 y의 개수.java
@@ -0,0 +1,17 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        int p = 0;
+        int y = 0;
+        char c;
+        for(int i=0; i<s.length(); i++){
+            c = s.charAt(i);
+            if(c == 'p'|| c == 'P') p++;
+            if(c == 'y' || c == 'Y') y++;                
+        }
+        if(p == y) answer = true;
+        else answer = false;
+        
+        return answer;
+    }
+}
