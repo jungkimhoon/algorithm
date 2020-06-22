59c7ebb3ba0ee1f254c2c6379c8bfdaa17af3317
12345 <ing2895@naver.com>
2020-06-22 16:08:16 +0900

    백준_dp

---
+++ baekjoon/다이나믹 프로그래밍/11726 2xn 타일링.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {		
+	public static void main(String[] args) throws IOException  {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] d = new int[n];
+		d[0] = 1;
+		
+		if(n>1) {
+			d[1] = 2;
+			for(int i=2; i<n; i++) {
+				d[i] = (d[i-1] + d[i-2])%10007;
+			}
+		}
+		
+		System.out.println(d[n-1]);
+		
+	}
+}
