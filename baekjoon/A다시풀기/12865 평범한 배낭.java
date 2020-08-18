fb24e7108ca31a2b822e8439d263c3dbc3cec307
12345 <ing2895@naver.com>
2020-08-18 19:25:03 +0900

    no message

---
+++ baekjoon/A다시풀기/12865 평범한 배낭.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {	
+		
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		int[][] dp = new int[n+1][k+1];
+		
+		int[] w = new int[n+1];
+		int[] v = new int[n+1];
+		
+		
+		for(int i=1; i<=n; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			w[i] = Integer.parseInt(st.nextToken());
+			v[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=k; j++) {
+				dp[i][j] = dp[i-1][j];
+				if(j - w[i] > 0) {
+					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-w[i]] + v[i]);
+				}				
+			}			
+		}
+		
+		System.out.println(dp[n][k]);		
+	}	
+}
