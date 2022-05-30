bd144fbeff69fc081bcd32efb66c9b24a9f664b9
jungkimhoon <ing2895@naver.com>
2022-05-30 21:05:43 +0900

    baekjoon-2941

---
+++ algorithm-ct/Test2941.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Test2941 {
+    final static String[] CROATIAN = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        new Test2941().solve();
+    }
+
+    public void solve() throws IOException {
+        String str = br.readLine();
+
+        for (int i = 0; i < CROATIAN.length; i++) {
+            str = str.replaceAll(CROATIAN[i], "t");
+        }
+
+        System.out.println(str.length());
+    }
+}
