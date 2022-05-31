9e7db17569911cfa166ce7838fa30dd63eaf3bd9
X <59942147+jungkimhoon@users.noreply.github.com>
2022-05-31 19:53:14 +0900

    Create Test2839.java
    baekjoon 2839

---
+++ algorithm-ct/Test2839.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Test2839 {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    final static int[] sugarBag = {3, 5};
+
+    public static void main(String[] args) throws IOException {
+        new Test2839().solve();
+    }
+
+
+    public void solve() throws IOException {
+        String str = br.readLine();
+        int n = Integer.parseInt(str);
+        int ret = -1;
+
+        for (int i = 0; i <= n / sugarBag[1]; i++) {
+            int a = i * sugarBag[1];
+
+            for (int j = 0; j <= n / sugarBag[0]; j++) {
+                int b = j * sugarBag[0];
+
+                if (a + b == n) {
+                    ret = i + j;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(ret);
+    }
+}
