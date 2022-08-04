bd1b91c8e02f4699513b551d91af2dfa8e9421f0
김정훈 <junghoon.kim@innowireless.com>
2022-08-04 10:47:39 +0900

    baekjoon 2294

---
+++ algorithm-ct/answer/Test2294.java
@@ -0,0 +1,36 @@
+package answer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Test2294 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+
+
+        int[] coin = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[k + 1];
+        Arrays.fill(dp, 100001);
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = coin[i]; j <= k; j++) {
+                dp[j] = Math.min(dp[j], dp[j - coin[i]] + 1);
+            }
+        }
+
+        System.out.println(dp[k] == 100001 ? -1 : dp[k]);
+    }
+}
